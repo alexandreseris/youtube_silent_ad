48e5c6b1e704765fe6683647b959672182715a51
alexandreseris <alexandreseris@hotmail.fr>
2022-01-23 20:22:48 +0100

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+if (document.location.host === "www.youtube.com") {
+  let currentlyPlayingAd = false
+  function loop() {
+    setTimeout(() => {
+      console.log("looping")
+
+      let banners = Array.from(document.querySelectorAll(".ytp-ad-text-overlay .ytp-ad-overlay-close-container, .ytp-ad-image-overlay .ytp-ad-overlay-close-container"))
+      let videoAdBanner = document.querySelector(".ytp-ad-player-overlay-instream-info")
+
+      if (banners.length > 0) {
+        console.log("banner ad (text or image) found")
+        Array.from(banners).forEach((e) => {e.click()})
+      }
+
+      if (videoAdBanner !== null) {
+        console.log("video ad found")
+        if (!currentlyPlayingAd) {
+          document.querySelector("video").muted = true
+          currentlyPlayingAd = true
+        }
+        let skipAd = document.querySelector("button.ytp-ad-skip-button")
+        if (skipAd) {
+          skipAd.click()
+        }
+      } else {
+        if (currentlyPlayingAd) {
+          document.querySelector("video").muted = false
+          currentlyPlayingAd = false
+        }
+      }
+
+      loop()
+    }, 500)
+  }
+  loop()
+}
