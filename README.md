a78f2482f5ea9fd779555a905d43210f4ca5f309
alexandreseris <alexandreseris@hotmail.fr>
2024-04-20 19:56:22 +0200

    update for newer youtube and using MutationObserver for better reactivity

--- README.md
+++ README.md
@@ -2,13 +2,6 @@
 
 *Mute video ads and skip them when possible. close text and image banner on Youtube*
 
-## CLOSED PROJET
-Does not work whith the newer versions of youtube and the site is smart enough to skip the add when using devtools
-
-If you have an idea on how to get around this behaviour, you can open a PR or an issue :)
-
----------------------------------------------
-
 [Addon link](https://addons.mozilla.org/fr/firefox/addon/youtube-silent-ad/)
 
 ## Build
--- main.js
+++ main.js
@@ -1,32 +1,24 @@
 if (document.location.host === "www.youtube.com") {
-  let currentlyPlayingAd = false
-  function loop() {
-    setTimeout(() => {
-      let banners = Array.from(document.querySelectorAll(".ytp-ad-text-overlay .ytp-ad-overlay-close-container, .ytp-ad-image-overlay .ytp-ad-overlay-close-container"))
-      let videoAdBanner = document.querySelector(".ytp-ad-player-overlay-instream-info")
+    let addPlaying = false
+    let rootElem = document.querySelector("ytd-app")
+    /**
+     * 
+     * @param {MutationRecord[]} mutations 
+     * @param {MutationObserver} observer 
+     */
+    function muteAd(mutations, observer) {
+        let adBanner = document.querySelector(".ytp-ad-text")
+        let videoPlayer = document.querySelector("video[src^=blob]")
 
-      if (banners.length > 0) {
-        Array.from(banners).forEach((e) => {e.click()})
-      }
-
-      if (videoAdBanner !== null) {
-        if (!currentlyPlayingAd) {
-          document.querySelector("video").muted = true
-          currentlyPlayingAd = true
+        if (videoPlayer !== null && adBanner !== null && !addPlaying) {
+            videoPlayer.muted = true
+            addPlaying = true
+        } else if (videoPlayer !== null && adBanner === null && addPlaying) {
+            videoPlayer.muted = false
+            addPlaying = false
         }
-        let skipAd = document.querySelector("button.ytp-ad-skip-button")
-        if (skipAd) {
-          skipAd.click()
-        }
-      } else {
-        if (currentlyPlayingAd) {
-          document.querySelector("video").muted = false
-          currentlyPlayingAd = false
-        }
-      }
-
-      loop()
-    }, 500)
-  }
-  loop()
+    }
+    // check https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+    let observer = new MutationObserver(muteAd)
+    observer.observe(rootElem, { childList: true, subtree: true })
 }
